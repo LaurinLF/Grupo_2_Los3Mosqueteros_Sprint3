984db268c3548764179a006efbcfc782ff018173
franlopezcopÂ <franlopcopp@gmail.com>
2022-06-25 21:16:18 -0300

    html de login y register listos

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.use(express.static(publicPath))
 let viewsPath = path.resolve(__dirname, "./views")
 app.get("/",(req,res) => res.sendFile(path.join(viewsPath,"./productDetail.html")))
 
+app.get('/register',(req,res) => res.sendFile(path.join(viewsPath,"./register.html")))
+app.get('/login',(req,res) => res.sendFile(path.join(viewsPath,"./login.html")))
 
 
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}!`));
