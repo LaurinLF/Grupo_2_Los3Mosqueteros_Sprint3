3af5af1c6a634fcbbbd6624ae57d5d1ca4e80560
franlopezcopÂ <franlopcopp@gmail.com>
2022-07-05 21:52:23 -0300

    gitignore y heroku

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 3030
+const port = process.env.PORT || 3000;
 
 let publicPath = path.resolve(__dirname, "./public")
 app.use(express.static(publicPath))
 
 let viewsPath = path.resolve(__dirname, "./views")
-app.get("/",(req,res) => res.sendFile(path.join(viewsPath,"./productDetail.html")))
+app.get("/",(req,res) => res.sendFile(path.join(viewsPath,"./home.html")))
+
+app.get("/detalle",(req,res) => res.sendFile(path.join(viewsPath,"./productDetail.html")))
 
 app.get('/register',(req,res) => res.sendFile(path.join(viewsPath,"./register.html")))
 app.get('/login',(req,res) => res.sendFile(path.join(viewsPath,"./login.html")))
 
 
-app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}!`));
+app.listen(port, () => {
+    console.log("Servidor corriendo en el puerto" + port);
+})
