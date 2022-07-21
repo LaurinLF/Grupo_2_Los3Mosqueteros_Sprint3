3649c377f62de21875acc810740b0dc7d7633584
LaurinLF <laura_lf20@hotmail.com>
2022-07-20 22:54:10 -0300

    ProductCart y dataBase

--- src/database/products.js
+++ src/database/products.js
@@ -1,8 +1,39 @@
-const products = [{
+const products = [{ 
+    id: 1,
     nombre: "Escritorio X" ,
-    precio: "$20.000",
-
-}];
-
+    precio: "$40.000",
+    "descripcion": "Escritorio hecho con madera refinada y soporte de hierro en forma de X. Con un atractivo diseño, este modelo combina lo fino y delicado con el estilo industrial e informal, ideal para cualquier espacio de la casa.",
+    "image": "escritorio.jpg",
+    "Medidas": "74cm de altura - 60 cm de ancho - 120cm de largo.",
+    "Descuento": "10",
+},
+{
+    id: 2,
+    nombre: "Mesa Cruz Guayubirá" ,
+    precio: "$130.000",
+    "descripcion": "Mesa de comedor de hierro y madera estilo industrial",
+    "image": "mesa-x.jpg",
+    "Medidas": "1.20mts de largo - 70cm de ancho - 75cm de alto.",
+    "Descuento": "15",
+},
+{
+    id: 3,
+    nombre: "Mesa Ratona" ,
+    precio: "$13.500",
+    "descripcion": "Mesa ratona estilo industrial, realizada con hierro y madera de pino",
+    "image": "mesa-ratona.jpg",
+    "Medidas": "121cm de largo - 50cm de ancho x 47cm de alto.",
+    "Descuento": "5",
+},
+{
+    id: 4,
+    nombre: "Espejo Blackfe" ,
+    precio: "$21.900",
+    "descripcion": "Espejo con marco y recuadros de hierro estilo industrial",
+    "image": "espejo.jpg",
+    "Medidas": "44cm de largo - 10cm de profundidad",
+    "Descuento": "10",
+},
+]
 
 module.exports = products
